3d726e3b43b5e5b76b69e74eb4d8fa91a89d0649
MNMF <mmonsalve15@alumnos.utalca.cl>
2021-10-20 21:22:14 -0300

    cmd estilo matrix basico

---
+++ README.md
@@ -0,0 +1,3 @@
+# matrix.bat
+
+![image](https://user-images.githubusercontent.com/50932052/138187333-a7da0aa9-5c3c-40e7-9278-f0f797b5f7c6.gif)
---
+++ matrix.bat
@@ -0,0 +1,13 @@
+@echo off 
+:start
+color 01 
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+color 02
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+color 04
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+color 05 
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+color 06
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto :start
